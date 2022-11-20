b859485617d8b3721c6047664fc64ef3c54d5206
xKronos58 <85923609+xKronos58@users.noreply.github.com>
2022-11-20 23:36:29 +1100

    base game

---
+++ Game/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static Game.CNSLart;
+
+namespace Game
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CNSLart art = new CNSLart();
+            charCreation cha = new charCreation();
+            
+            //Player stats
+
+            double hp;
+            long chips;
+
+            Random rnd = new Random();
+            
+            //Title screen
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+              Console.WriteLine(
+                      "\n" + art.titleArt[0] + "\n" + art.titleArt[1] 
+                    + "\n" + art.titleArt[2] + "\n" + art.titleArt[3] 
+                    + "\n" + art.titleArt[4] + "\n" + art.titleArt[5]
+                    + "\n" + art.titleArt[6] + "\n" + art.titleArt[7]
+                    + "\n" + art.titleArt[8] + "\n" + art.titleArt[9]);
+              Console.ResetColor();
+              Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\nWelcome to GAME NAME");
+            Console.ResetColor();
+            
+            //Character creation
+            
+            Console.WriteLine("\n\n\nDo you wish to create your own character or have a randomised one?\n(Type \"rand\", \"prsts\" for a list of presets or \"cstm\" for a custom one)");
+            inchrC: ;
+            String chrC = Console.ReadLine().ToLower();
+            if (chrC == "rand")
+            {
+                cha.randomChar();
+            } else if (chrC == "prsts")
+            {
+                cha.presetChar();
+            } else if (chrC == "cstm")
+            {
+                cha.customChar();
+            } else if (chrC == null || chrC != "rand" || chrC != "prsts" || chrC != "cstm")
+            {
+                Console.WriteLine("There was a null or incorrect value\nPlease try again");
+                goto inchrC;
+            }
+
+            //Game Loop
+        }
+    }
+}
---
+++ Game/charCreation.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+
+namespace Game
+{
+    public class charCreation
+    {
+        private Random rnd = new Random();
+
+        Program p = new Program();
+
+        public byte Class;
+        
+        int[] visAtt =
+        {
+            0, //head           |   0   |
+            0, //torso          |   1   |
+            0, //arms           |   2   |
+            0, //legs           |   3   |
+            0, //hands          |   4   |
+            0, //shoes &! Feet  |   5   |
+        };
+        
+        private byte randAttri;
+
+        private string cla;
+        
+        public void randomChar()
+        {
+            Class = (byte)rnd.Next(1,8);
+            visAtt[0] = (int)rnd.Next(1, 6);
+            visAtt[1] = (int)rnd.Next(1, 6);
+            visAtt[2] = (int)rnd.Next(1, 6);
+            visAtt[3] = (int)rnd.Next(1, 6);
+            visAtt[4] = (int)rnd.Next(1, 6);
+            visAtt[5] = (int)rnd.Next(1, 6);
+            switch (Class)
+            {
+                case 1:
+                    cla = Class1[0];
+                    break;
+                case 2:
+                    cla = Class2[0];
+                    break;
+                case 3:
+                    cla = Class3[0];
+                    break;
+                case 4:
+                    cla = Class4[0];
+                    break;
+                case 5:
+                    cla = Class5[0];
+                    break;
+                case 6:
+                    cla = Class6[0];
+                    break;
+                case 7:
+                    cla = Class7[0];
+                    break;
+                case 8:
+                    cla = Class8[0];
+                    break;
+            }
+            
+            saveRace();
+        }
+        
+        private void saveRace()
+        {
+            string time = DateTime.Now.ToString("h.mm.ss");
+            string path = @"W:\\C#_code\\GameLol\\Game\\Game\\saveFiles\\save_" + time + ".txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Class: " + cla + " (" + Class + ")");
+                    sw.WriteLine("head :" + visAtt[0]);
+                    sw.WriteLine("torso:" + visAtt[1]);
+                    sw.WriteLine("arms :" + visAtt[2]);
+                    sw.WriteLine("legs :" + visAtt[3]);
+                    sw.WriteLine("hands:" + visAtt[4]);
+                    sw.WriteLine("feet :" + visAtt[5]);
+                    sw.WriteLine(time);
+                }
+            }
+        }
+
+        public void presetChar()
+        {
+            
+        }
+
+        public void customChar()
+        {
+            
+        }
+
+        public string[] Class1 /* Regular */ =
+        {
+            "Regular",  // class name
+            "0",        // max HP
+            "0",        // Melee damage bonus
+            "0",        // Ranged damage bonus
+            "0",        // Cyber damage bonus
+            "0",        // Stamina
+            "0",        // Speed
+            "0"         // Luck
+        };
+        
+        public string[] Class2 /*  */ =
+        {
+            "---",  // class name
+            "0",        // max HP
+            "0",        // Melee damage bonus
+            "0",        // Ranged damage bonus
+            "0",        // Cyber damage bonus
+            "0",        // Stamina
+            "0",        // Speed
+            "0"         // Luck
+        };
+        
+        public string[] Class3 /*  */ =
+        {
+            "---",  // class name
+            "0",        // max HP
+            "0",        // Melee damage bonus
+            "0",        // Ranged damage bonus
+            "0",        // Cyber damage bonus
+            "0",        // Stamina
+            "0",        // Speed
+            "0"         // Luck
+        };
+        
+        public string[] Class4 /*  */ =
+        {
+            "---",  // class name
+            "0",        // max HP
+            "0",        // Melee damage bonus
+            "0",        // Ranged damage bonus
+            "0",        // Cyber damage bonus
+            "0",        // Stamina
+            "0",        // Speed
+            "0"         // Luck
+        };
+        
+        public string[] Class5 /*  */ =
+        {
+            "---",  // class name
+            "0",        // max HP
+            "0",        // Melee damage bonus
+            "0",        // Ranged damage bonus
+            "0",        // Cyber damage bonus
+            "0",        // Stamina
+            "0",        // Speed
+            "0"         // Luck
+        };
+        
+        public string[] Class6 /*  */ =
+        {
+            "---",  // class name
+            "0",        // max HP
+            "0",        // Melee damage bonus
+            "0",        // Ranged damage bonus
+            "0",        // Cyber damage bonus
+            "0",        // Stamina
+            "0",        // Speed
+            "0"         // Luck
+        };
+        
+        public string[] Class7 /*  */ =
+        {
+            "---",  // class name
+            "0",        // max HP
+            "0",        // Melee damage bonus
+            "0",        // Ranged damage bonus
+            "0",        // Cyber damage bonus
+            "0",        // Stamina
+            "0",        // Speed
+            "0"         // Luck
+        };
+        
+        public string[] Class8 /*  */ =
+        {
+            "---",  // class name
+            "0",        // max HP
+            "0",        // Melee damage bonus
+            "0",        // Ranged damage bonus
+            "0",        // Cyber damage bonus
+            "0",        // Stamina
+            "0",        // Speed
+            "0"         // Luck
+        };
+    }
+}
