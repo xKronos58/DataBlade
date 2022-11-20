8ff773c9a547b2cf0a07ac2deef0a99f7c51b381
xKronos58 <finleycrowther@outlook.com>
2022-11-21 10:21:25 +1100

    Added Char presets (Still needs the presets)

---
+++ Game/CharPresets.cs
@@ -0,0 +1,101 @@
+namespace Game
+{
+    public class CharPresets
+    {
+        public int[] preset1 =
+        {
+            0, //class	    	|   0   |
+            0, //head           |   1   |
+            0, //torso          |   2   |
+            0, //arms           |   3   |
+            0, //legs           |   4   |
+            0, //hands          |   5   |
+            0, //shoes &! Feet  |   6   |
+            1, //Preset num 	|   7   |
+        };
+
+        public int[] preset2 =
+        {
+            0, //class	    	|   0   |
+            0, //head           |   1   |
+            0, //torso          |   2   |
+            0, //arms           |   3   |
+            0, //legs           |   4   |
+            0, //hands          |   5   |
+            0, //shoes &! Feet  |   6   | 
+            2, //Preset num 	|   7   |
+        };
+
+        public int[] preset3 =
+        {
+            0, //class	    	|   0   |
+            0, //head           |   1   |
+            0, //torso          |   2   |
+            0, //arms           |   3   |
+            0, //legs           |   4   |
+            0, //hands          |   5   |
+            0, //shoes &! Feet  |   6   |
+            3, //Preset num 	|   7   |
+        };
+
+        public int[] preset4 =
+        {
+            0, //class	    	|   0   |
+            0, //head           |   1   |
+            0, //torso          |   2   |
+            0, //arms           |   3   |
+            0, //legs           |   4   |
+            0, //hands          |   5   |
+            0, //shoes &! Feet  |   6   |
+            4, //Preset num 	|   7   |
+        };
+        
+        public int[] preset5 =
+        {
+            0, //class	    	|   0   |
+            0, //head           |   1   |
+            0, //torso          |   2   |
+            0, //arms           |   3   |
+            0, //legs           |   4   |
+            0, //hands          |   5   |
+            0, //shoes &! Feet  |   6   |
+            5, //Preset num 	|   7   |
+        };
+        
+        public int[] preset6 =
+        {
+            0, //class	    	|   0   |
+            0, //head           |   1   |
+            0, //torso          |   2   |
+            0, //arms           |   3   |
+            0, //legs           |   4   |
+            0, //hands          |   5   |
+            0, //shoes &! Feet  |   6   |
+            6, //Preset num 	|   7   |
+        };
+        
+        public int[] preset7 =
+        {
+            0, //class	    	|   0   |
+            0, //head           |   1   |
+            0, //torso          |   2   |
+            0, //arms           |   3   |
+            0, //legs           |   4   |
+            0, //hands          |   5   |
+            0, //shoes &! Feet  |   6   |
+            7, //Preset num 	|   7   |
+        };
+        
+        public int[] preset8 =
+        {
+            0, //class	    	|   0   |
+            0, //head           |   1   |
+            0, //torso          |   2   |
+            0, //arms           |   3   |
+            0, //legs           |   4   |
+            0, //hands          |   5   |
+            0, //shoes &! Feet  |   6   |
+            8, //Preset num 	|   7   |
+        };
+    }
+}
