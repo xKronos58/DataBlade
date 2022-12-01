4b8e5645c71fb206d1dbf73a90a97c1e5058c3fd
xKronos58 <finleycrowther@outlook.com>
2022-12-01 22:17:47 +1100

    Fixed load func and cleaned up code

--- Game/LoadFunc.cs
+++ Game/LoadFunc.cs
@@ -139,57 +139,16 @@ namespace Game
                                 srbs.Read(d, 0, 9);
                                 var bar = Char.GetNumericValue(d[7]);
                                 double classS = bar;
-                                Console.WriteLine(classS);
+                                Console.WriteLine("DoubleVal:" + classS);
                             }
                         }
                     }
-                    string[] savess1 = File.ReadAllLines(timeS[2]);
-                    char[] b1 = new char[savess.Length];
-                    int savessL1 = 0;
-                    savess1.GetLength(savessL1);
-                
                     using (StringReader sr1 = new StringReader(savess[1]))
                     {
-                        sr1.Read(b1, 0, 9);
-                        Console.WriteLine(b1);
-                        char[] bs1 = new char[b1.Length];
-                        using (StringReader srb = new StringReader(b1[7].ToString()))
-                        {
-                            unsafe
-                            {
-                                srb.Read(bs1, 0, 1);
-                                // Console.WriteLine(bs1);
-                                string bss = bs1.ToString();
-                                if (bs1 != null)
-                                {
-                                    int bsn = bs1.Length; 
-                                    Debug.Assert(bs1 != null, nameof(bs1) + " != null");
-                                    string[] bsss = new[] { bs1[bsn - 1].ToString() };
-                                }
-                                char[] d = new char[savess.Length];
-                                string[] c = new string[] {};
-
-                                using (StringReader srbs = new StringReader(savess[0]))
-                                {
-                                    srbs.Read(d, 0, 9);
-                                    var bar = Char.GetNumericValue(d[7]);
-                                    double classS = bar;
-                                    Console.WriteLine(classS);
-                                }
-                            }
-                        }
-                    }
-                    string[] savess2 = File.ReadAllLines(timeS[2]);
-                    char[] b2 = new char[savess2.Length];
-                    int savessL2 = 0;
-                    savess2.GetLength(savessL2);
-                
-                    using (StringReader sr2 = new StringReader(savess[2]))
-                    {
-                        sr2.Read(b2, 0, 9);
-                        Console.WriteLine(b2);
-                        char[] bs2 = new char[b2.Length];
-                        using (StringReader srb = new StringReader(b2[7].ToString()))
+                        sr1.Read(b, 0, 9);
+                        Console.WriteLine(b);
+                        char[] bs2 = new char[b.Length];
+                        using (StringReader srb = new StringReader(b[7].ToString()))
                         {
                             unsafe
                             {
@@ -205,27 +164,53 @@ namespace Game
                                 char[] d = new char[savess.Length];
                                 string[] c = new string[] {};
 
-                                using (StringReader srbs = new StringReader(savess[0]))
+                                using (StringReader srbs = new StringReader(savess[1]))
                                 {
                                     srbs.Read(d, 0, 9);
                                     var bar = Char.GetNumericValue(d[7]);
                                     double classS = bar;
-                                    Console.WriteLine(classS);
+                                    Console.WriteLine("DoubleVal:" + classS);
                                 }
                             }
                         }
                     }
-                    string[] savess3 = File.ReadAllLines(timeS[2]);
-                    char[] b3 = new char[savess3.Length];
-                    int savessL3 = 0;
-                    savess3.GetLength(savessL3);
-                
+                    using (StringReader sr2 = new StringReader(savess[2]))
+                    {
+                        sr2.Read(b, 0, 9);
+                        Console.WriteLine(b);
+                        char[] bs2 = new char[b.Length];
+                        using (StringReader srb = new StringReader(b[7].ToString()))
+                        {
+                            unsafe
+                            {
+                                srb.Read(bs2, 0, 1);
+                                // Console.WriteLine(bs2);
+                                string bss = bs2.ToString();
+                                if (bs2 != null)
+                                {
+                                    int bsn = bs2.Length; 
+                                    Debug.Assert(bs2 != null, nameof(bs2) + " != null");
+                                    string[] bsss = new[] { bs2[bsn - 1].ToString() };
+                                }
+                                char[] d = new char[savess.Length];
+                                string[] c = new string[] {};
+
+                                using (StringReader srbs = new StringReader(savess[2]))
+                                {
+                                    srbs.Read(d, 0, 9);
+                                    var bar = Char.GetNumericValue(d[7]);
+                                    double classS = bar;
+                                    Console.WriteLine("DoubleVal:" + classS);
+                                }
+                            }
+                        }
+                    }
                     using (StringReader sr3 = new StringReader(savess[3]))
                     {
-                        sr3.Read(b3, 0, 9);
-                        Console.WriteLine(b3);
-                        char[] bs3 = new char[b3.Length];
-                        using (StringReader srb = new StringReader(b3[7].ToString()))
+                        sr3.Read(b, 0, 9);
+                        Console.WriteLine(b);
+                        char[] bs3 = new char[b.Length];
+                        using (StringReader srb = new StringReader(b[7].ToString()))
                         {
                             unsafe
                             {
@@ -241,27 +226,22 @@ namespace Game
                                 char[] d = new char[savess.Length];
                                 string[] c = new string[] {};
 
-                                using (StringReader srbs = new StringReader(savess[0]))
+                                using (StringReader srbs = new StringReader(savess[3]))
                                 {
                                     srbs.Read(d, 0, 9);
                                     var bar = Char.GetNumericValue(d[7]);
                                     double classS = bar;
-                                    Console.WriteLine(classS);
+                                    Console.WriteLine("DoubleVal:" + classS);
                                 }
                             }
                         }
                     }
-                    string[] savess4 = File.ReadAllLines(timeS[2]);
-                    char[] b4 = new char[savess4.Length];
-                    int savessL4 = 0;
-                    savess4.GetLength(savessL4);
-                
                     using (StringReader sr4 = new StringReader(savess[4]))
                     {
-                        sr4.Read(b4, 0, 9);
-                        Console.WriteLine(b4);
-                        char[] bs4 = new char[b4.Length];
-                        using (StringReader srb = new StringReader(b4[7].ToString()))
+                        sr4.Read(b, 0, 9);
+                        Console.WriteLine(b);
+                        char[] bs4 = new char[b.Length];
+                        using (StringReader srb = new StringReader(b[7].ToString()))
                         {
                             unsafe
                             {
@@ -277,27 +257,22 @@ namespace Game
                                 char[] d = new char[savess.Length];
                                 string[] c = new string[] {};
 
-                                using (StringReader srbs = new StringReader(savess[0]))
+                                using (StringReader srbs = new StringReader(savess[4]))
                                 {
                                     srbs.Read(d, 0, 9);
                                     var bar = Char.GetNumericValue(d[7]);
                                     double classS = bar;
-                                    Console.WriteLine(classS);
+                                    Console.WriteLine("DoubleVal:" + classS);
                                 }
                             }
                         }
                     }
-                    string[] savess5 = File.ReadAllLines(timeS[5]);
-                    char[] b5 = new char[savess5.Length];
-                    int savessL5 = 0;
-                    savess5.GetLength(savessL5);
-                
                     using (StringReader sr5 = new StringReader(savess[5]))
                     {
-                        sr5.Read(b5, 0, 9);
-                        Console.WriteLine(b5);
-                        char[] bs5 = new char[b5.Length];
-                        using (StringReader srb = new StringReader(b5[7].ToString()))
+                        sr5.Read(b, 0, 9);
+                        Console.WriteLine(b);
+                        char[] bs5 = new char[b.Length];
+                        using (StringReader srb = new StringReader(b[7].ToString()))
                         {
                             unsafe
                             {
@@ -313,27 +288,22 @@ namespace Game
                                 char[] d = new char[savess.Length];
                                 string[] c = new string[] {};
 
-                                using (StringReader srbs = new StringReader(savess[0]))
+                                using (StringReader srbs = new StringReader(savess[5]))
                                 {
                                     srbs.Read(d, 0, 9);
                                     var bar = Char.GetNumericValue(d[7]);
                                     double classS = bar;
-                                    Console.WriteLine(classS);
+                                    Console.WriteLine("DoubleVal:" + classS);
                                 }
                             }
                         }
                     }
-                    string[] savess6 = File.ReadAllLines(timeS[2]);
-                    char[] b6 = new char[savess6.Length];
-                    int savessL6 = 0;
-                    savess6.GetLength(savessL6);
-                
                     using (StringReader sr6 = new StringReader(savess[6]))
                     {
-                        sr6.Read(b6, 0, 9);
-                        Console.WriteLine(b6);
-                        char[] bs6 = new char[b6.Length];
-                        using (StringReader srb = new StringReader(b6[7].ToString()))
+                        sr6.Read(b, 0, 9);
+                        Console.WriteLine(b);
+                        char[] bs6 = new char[b.Length];
+                        using (StringReader srb = new StringReader(b[7].ToString()))
                         {
                             unsafe
                             {
@@ -349,31 +319,26 @@ namespace Game
                                 char[] d = new char[savess.Length];
                                 string[] c = new string[] {};
 
-                                using (StringReader srbs = new StringReader(savess[0]))
+                                using (StringReader srbs = new StringReader(savess[6]))
                                 {
                                     srbs.Read(d, 0, 9);
                                     var bar = Char.GetNumericValue(d[7]);
                                     double classS = bar;
-                                    Console.WriteLine(classS);
+                                    Console.WriteLine("DoubleVal:" + classS);
                                 }
                             }
                         }
                     }
-                    string[] savess7 = File.ReadAllLines(timeS[2]);
-                    char[] b7 = new char[savess7.Length];
-                    int savessL7 = 0;
-                    savess7.GetLength(savessL7);
-                
-                    using (StringReader sr7 = new StringReader(savess7[7]))
+                    using (StringReader sr7 = new StringReader(savess[7]))
                     {
-                        sr7.Read(b7, 0, 9);
-                        Console.WriteLine(b7);
-                        char[] bs7 = new char[b7.Length];
-                        using (StringReader srb = new StringReader(b7[7].ToString()))
+                        sr7.Read(b, 0, 9);
+                        Console.WriteLine(b);
+                        char[] bs7 = new char[b.Length];
+                        using (StringReader srb7 = new StringReader(b[7].ToString()))
                         {
                             unsafe
                             {
-                                srb.Read(bs7, 0, 1);
+                                srb7.Read(bs7, 0, 1);
                                 // Console.WriteLine(bs7);
                                 string bss = bs7.ToString();
                                 if (bs7 != null)
@@ -382,15 +347,15 @@ namespace Game
                                     Debug.Assert(bs7 != null, nameof(bs7) + " != null");
                                     string[] bsss = new[] { bs7[bsn - 1].ToString() };
                                 }
-                                char[] d = new char[savess7.Length];
+                                char[] d = new char[savess.Length];
                                 string[] c = new string[] {};
 
-                                using (StringReader srbs = new StringReader(savess7[7]))
+                                using (StringReader srbs = new StringReader(savess[7]))
                                 {
                                     srbs.Read(d, 0, 9);
                                     var bar = Char.GetNumericValue(d[7]);
                                     double classS = bar;
-                                    Console.WriteLine(classS);
+                                    Console.WriteLine("DoubleVal:" + classS);
                                 }
                             }
                         }
